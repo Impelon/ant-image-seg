8b0d7ad77126c0fcdb84d885ab3f056d129c3a3a
Impelon <ovi.tatar@googlemail.com>
2023-05-09 12:54:43 +0200

    Add cosine-similarity color distance

--- src/image_arithmetic/color_distances.rs
+++ src/image_arithmetic/color_distances.rs
@@ -1,7 +1,11 @@
 use image::Rgb;
 
+fn multiply(x: u8, y: u8) -> f64 {
+    ((x as u16) * (y as u16)) as f64
+}
+
 fn square(x: u8) -> f64 {
-    ((x as u16) * (x as u16)) as f64
+    multiply(x, x)
 }
 
 pub fn euclidean_squared(a: &Rgb<u8>, b: &Rgb<u8>) -> f64 {
@@ -18,4 +22,16 @@ fn absdiff(a: u8, b: u8) -> f64 {
 
 pub fn manhattan(a: &Rgb<u8>, b: &Rgb<u8>) -> f64 {
     absdiff(a.0[0], b.0[0]) + absdiff(a.0[1], b.0[1]) + absdiff(a.0[2], b.0[2])
+}
+
+fn magnitude(x: &Rgb<u8>) -> f64 {
+    (square(x.0[0]) + square(x.0[1]) + square(x.0[2])).sqrt()
+}
+
+pub fn cosine_unnormed(a: &Rgb<u8>, b: &Rgb<u8>) -> f64 {
+    multiply(a.0[0], b.0[0]) + multiply(a.0[1], b.0[1]) + multiply(a.0[2], b.0[2])
+}
+
+pub fn cosine(a: &Rgb<u8>, b: &Rgb<u8>) -> f64 {
+    cosine_unnormed(a, b) / (magnitude(a) * magnitude(b))
 }
